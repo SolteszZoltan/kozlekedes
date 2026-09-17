8dfd0a10657ebe45bb08631c0207869f5134314a
SolteszZoltan <solteszz@kkszki.hu>
2026-09-17 11:05:52 +0200

    Add project files.

---
+++ kozlekedes/Vonalbusz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kozlekedes
+{
+    class Vonalbusz : Utazas
+    {
+        public Vonalbusz(double alapdij, double tavolsag)
+            : base(alapdij, tavolsag)
+        {
+        }
+
+        public override double arSzamitas()
+        {
+            return alapdij + tavolsag * 30;
+        }
+    }
+}
---
+++ kozlekedes/utazas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kozlekedes
+{
+    abstract class Utazas
+    {
+        protected double alapdij;
+        protected double tavolsag;
+
+        public Utazas(double alapdij, double tavolsag)
+        {
+            this.alapdij = alapdij;
+            this.tavolsag = tavolsag;
+        }
+
+        public abstract double arSzamitas();
+    }
+
+    internal class utazas
+    {
+    }
+}
